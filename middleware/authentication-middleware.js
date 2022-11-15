6726af9cec77bde5d01f0994945fc5994869391a
ahmadmurteza <59716267+ahmadmurteza@users.noreply.github.com>
2022-11-15 12:30:41 +0800

    token

--- middleware/authentication-middleware.js
+++ middleware/authentication-middleware.js
@@ -3,11 +3,7 @@ const { User } = require("./../models/index");
 
 async function authenticationMiddleware(req, res, next) {
 	const  {authorization}  = req.headers;
-	if (authorization) {
-		const token = authorization.split("Bearer ");	
-	} else {
-		const token = '';
-	}
+	const token = authorization && authorization.split("Bearer ");
 	try {
 		if (token.length !== 2) throw { name: "InvalidToken" };
 		const { id, email } = verify(token[1]);
